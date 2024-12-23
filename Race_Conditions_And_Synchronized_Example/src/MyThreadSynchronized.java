cc8cfc7fbf7b974b3b121a66bde63d76f47bacd5
Roman Manzhula <roman1985al@ukr.net>
2024-12-23 21:55:50 +0200

    Update MyThreadSynchronized.java
    In Ukrainian

--- Race_Conditions_And_Synchronized_Example/src/MyThreadSynchronized.java
+++ Race_Conditions_And_Synchronized_Example/src/MyThreadSynchronized.java
@@ -1,22 +1,22 @@
 import java.util.ArrayList;
 
 public class MyThreadSynchronized extends Thread {
-    @Override //переопределяем метод run() интерфейса Runnable
+    @Override //перевизначаємо метод run() інтерфейсу Runnable
     public void run() {
-        generalMethodWithSynchronized(); //вызываем метод главныйМетодССинхронизацией()
+        generalMethodWithSynchronized(); //викликаємо метод generalMethodWithSynchronized()
     }
-    public synchronized void generalMethodWithSynchronized() { //обьявляем синхронизированный метод главныйМетодССинхронизацией()
-        ArrayList<String> list = new ArrayList<>(); //создаем список
-        list.add("I "); //добавляем в список строковый литерал
-        list.add("really "); //добавляем в список строковый литерал
-        list.add("like "); //добавляем в список строковый литерал
-        list.add("coding!"); //добавляем в список строковый литерал
-        printList(list); //вызываем метод печататьСписок(аргумент: наш список)
+    public synchronized void generalMethodWithSynchronized() { //оголошуємо синхронізований метод generalMethodWithSynchronized()
+        ArrayList<String> list = new ArrayList<>(); //створюємо список
+        list.add("I "); //додаємо до списку рядковий літерал
+        list.add("really "); //додаємо до списку рядковий літерал
+        list.add("like "); //додаємо до списку рядковий літерал
+        list.add("coding!"); //додаємо до списку рядковий літерал
+        printList(list); //викликаємо метод printList(аргумент: наш список)
 
     }
-    public synchronized void printList(ArrayList<String> list) { //обьявляем синхронизированный метод печататьСписок(параметр: список)
-        for (String str : list) { //циклом для каждого проходимся по списку
-            System.out.println(str + " - " + getName()); //печатаем каждый элемент списка + системное(автоматическое) имя потока
+    public synchronized void printList(ArrayList<String> list) { //оголошуємо синхронізований метод printList(параметр: список)
+        for (String str : list) { //циклом для кожного проходимося по списку
+            System.out.println(str + " - " + getName()); //виводимо кожен елемент списку + системне (автоматичне) ім'я потоку
         }
     }
 }
